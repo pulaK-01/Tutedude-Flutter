5b2b977fd0466ce535346ed79fee4c3f25deb3bd
pulaK_01 <pulak_01@icloud.com>
2026-08-25 15:37:13 +0530

    Update main.dart

--- assignment-4/first_app/lib/main.dart
+++ assignment-4/first_app/lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "My First App",
+  runApp(
+    MaterialApp(
+      title: "My First App",
 
-    home: Scaffold(
-      backgroundColor: Colors.amber,
-      appBar: AppBar(
-        title: Text("application's appbar"),
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
-      body: Center(
-          child: Text("My First App", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("application's appbar"),
+          backgroundColor: Colors.tealAccent,
+        ),
+        body: Center(
+          child: Text(
+            "My First App",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     ),
