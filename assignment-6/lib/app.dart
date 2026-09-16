57b8f07381c0cb2aa458913c086624f72f16f386
pulaK_01 <pulak_01@icloud.com>
2026-09-17 03:49:38 +0530

    assignment 6

---
+++ assignment-6/lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'screen/homescreen.dart';
+
+class Application6 extends StatelessWidget {
+  const Application6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Application6', home: Homescreen());
+  }
+}
--- assignment-6/lib/main.dart
+++ assignment-6/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'app.dart';
 
 void main() {
-  runApp(Application5());
+  runApp(Application6());
 }
-
